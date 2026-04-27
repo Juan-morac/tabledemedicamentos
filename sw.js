be8e75c695d35f7568f31b756772de72ecee02d1
Juan-morac <juanignacio9307@gmail.com>
2026-04-26 23:51:25 -0400

    Delete sw.js

--- sw.js
+++
@@ -1,4 +0,0 @@
-const CACHE='guia-ev-hcfb-v1';
-const ASSETS=['./','./index.html','./manifest.webmanifest','./assets/logo.png','./tablas-medicamentos-hcfb.pdf','./medicamentos-data.json'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
-self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
